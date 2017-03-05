fd8bd1fd556555029a879cccd8c541a7268a3d52
Denis Popov <dzutaro@gmail.com>
2017-03-06 00:57:26 +0300

    Indent code

--- app/modules/custom/reiscout_purchased_addresses/reiscout_purchased_addresses.js
+++ app/modules/custom/reiscout_purchased_addresses/reiscout_purchased_addresses.js
@@ -2,13 +2,12 @@
  * Implements hook_install().
  */
 function reiscout_purchased_addresses_install() {
-    try {
-        var css_file = drupalgap_get_path('module', 'reiscout_purchased_addresses') + '/reiscout_purchased_addresses.css';
-        drupalgap_add_css(css_file);
-    }
-    catch (error) {
-      console.log('reiscout_purchased_addresses_install - ' + error);
-    }
+  try {
+    drupalgap_add_css(drupalgap_get_path('module', 'reiscout_purchased_addresses') + '/reiscout_purchased_addresses.css');
+  }
+  catch (error) {
+    console.log('reiscout_purchased_addresses_install - ' + error);
+  }
 }
 
 /**
@@ -16,57 +15,59 @@ function reiscout_purchased_addresses_install() {
  * @return {Object}
  */
 function reiscout_purchased_addresses_menu() {
-    var items = {};
+  var items = {};
 
-    items['purchased-addresses'] = {
-        title: 'Purchased Addresses',
-        page_callback: 'reiscout_purchased_addresses_list_page'
-    };
+  items['purchased-addresses'] = {
+    title: 'Purchased Addresses',
+    page_callback: 'reiscout_purchased_addresses_list_page'
+  };
 
-    return items;
+  return items;
 }
 
 /**
  * The page callback to display the view.
  */
 function reiscout_purchased_addresses_list_page() {
-    try {
-        var content = {};
+  try {
+    var content = {};
 
-        content['address_list'] = {
-            theme: 'view',
-            format: 'ul',
-            path: 'drupalgap/views_datasource/purchased_address_list/' + Drupal.user.uid,
-            row_callback: 'reiscout_purchased_addresses_list_row',
-            empty_callback: 'reiscout_purchased_addresses_list_empty',
-            attributes: {
-                id: 'reiscout_purchased_addresses_list_view'
-            }
-        };
+    content['address_list'] = {
+      theme: 'view',
+      format: 'ul',
+      path: 'drupalgap/views_datasource/purchased_address_list/' + Drupal.user.uid,
+      row_callback: 'reiscout_purchased_addresses_list_row',
+      empty_callback: 'reiscout_purchased_addresses_list_empty',
+      attributes: {
+        id: 'reiscout_purchased_addresses_list_view'
+      }
+    };
 
-        return content;
-    }
-    catch (error) {
-      console.log('reiscout_purchased_addresses_list_page - ' + error);
-    }
+    return content;
+  }
+  catch (error) {
+    console.log('reiscout_purchased_addresses_list_page - ' + error);
+  }
 }
 
 /**
  * The row callback to render a single row.
  */
 function reiscout_purchased_addresses_list_row(view, row) {
-    try {
-        image = theme('image', {
-          path: row.image.src,
-          alt: row.image.alt
-        });
+  try {
+    image = theme('image', {
+      path: row.image.src,
+      alt: row.image.alt
+    });
 
-        title = l(t(row.address), 'node/' + row.nid);
+    title = l(t(row.address), 'node/' + row.nid);
 
-        return '<div class="image">' + image + '</div>'
-             + '<div class="title">' + title + '</div>';
-    }
-    catch (error) { console.log('reiscout_purchased_addresses_list_row - ' + error); }
+    return '<div class="image">' + image + '</div>'
+         + '<div class="title">' + title + '</div>';
+  }
+  catch (error) {
+    console.log('reiscout_purchased_addresses_list_row - ' + error);
+  }
 }
 
 /**
