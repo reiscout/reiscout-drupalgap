1b65e6b6270f89020fe924916986b430d02ecd0f
Denis Popov <dzutaro@gmail.com>
2017-06-08 17:57:56 +0300

    Task #674957: update commerce_drupalgap_stripe_form form

--- www/app/modules/custom/reiscout_property_commerce/reiscout_property_commerce.js
+++ www/app/modules/custom/reiscout_property_commerce/reiscout_property_commerce.js
@@ -55,8 +55,16 @@ function reiscout_property_commerce_form_alter(form, form_state, form_id) {
     }
     else if (form_id == 'commerce_drupalgap_stripe_form') {
       form.elements['card_cvc']['type'] = 'number';
+
       form.elements['exp_month']['type'] = 'number';
+      form.elements['exp_month']['prefix'] = '<label><strong>' + t('Expiration Date') + '</strong>*</label>';
+      form.elements['exp_month']['title'] = '';
+      form.elements['exp_month']['options']['attributes']['placeholder'] = t('MM');
+      form.elements['exp_month']['suffix'] = '/';
+
       form.elements['exp_year']['type'] = 'number';
+      form.elements['exp_year']['title'] = '';
+      form.elements['exp_year']['options']['attributes']['placeholder'] = t('YYYY');
     }
   }
   catch (error) {
