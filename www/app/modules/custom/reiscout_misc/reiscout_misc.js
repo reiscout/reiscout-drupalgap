7abe37614d939a2612b7b89cc634c72b88df8703
Denis Popov <dzutaro@gmail.com>
2017-07-19 23:45:54 +0300

    Feature #684606: redirect a user to the home page

--- www/app/modules/custom/reiscout_misc/reiscout_misc.js
+++ www/app/modules/custom/reiscout_misc/reiscout_misc.js
@@ -11,3 +11,31 @@ function reiscout_misc_form_alter(form, form_state, form_id) {
     console.log('reiscout_misc_form_alter - ' + error);
   }
 }
+
+/**
+ * Implements hook_services_preprocess().
+ * @param {Object} options
+ */
+function reiscout_misc_services_preprocess(options) {
+  try {
+    // This Services call is initiated by contact_site(). By default, success callback
+    // alerts a message to the user and clears all the fields on the contact form.
+    // But we want to redirect the user to the home page and show him the message.
+    if (options.service == 'contact' && options.resource == 'site') {
+      options.success = function(result) {
+        if (result[0]) {
+          drupalgap_set_message(t('Your message has been sent!'));
+        }
+        else {
+          var msg = t('Your message cannot be sent. Please, try again later or contact technical support for assistance!');
+          drupalgap_set_message(msg, 'error');
+        }
+        drupalgap_form_clear();
+        drupalgap_goto('property-listing', {reloadPage: true});
+      }
+    }
+  }
+  catch (error) {
+    console.log('reiscout_misc_services_preprocess - ' + error);
+  }
+}
