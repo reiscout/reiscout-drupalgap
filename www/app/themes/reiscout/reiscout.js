526213de589e543b26d8ac169b4cade883127411
Denis Popov <dzutaro@gmail.com>
2017-08-08 01:57:13 +0300

    Task #689175: display a product title instead of a product SKU on the 'Review Order' page

--- www/app/themes/reiscout/reiscout.js
+++ www/app/themes/reiscout/reiscout.js
@@ -72,6 +72,25 @@ function reiscout_commerce_cart_line_item(variables) {
 }
 
 /**
+ * Themes a commerce cart line item.
+ */
+function reiscout_commerce_cart_line_item_review(variables) {
+  try {
+    var quantity = Math.floor(variables.line_item.quantity);
+    var label = variables.line_item.line_item_title;
+    var html = '<h2>' + quantity + ' x ' + label  + '</h2>' +
+      '<p><strong>Price</strong>: ' + variables.line_item.commerce_unit_price_formatted + '</p>';
+    html += '<p class="ui-li-aside"><strong>Total</strong>: ' +
+      variables.line_item.commerce_total_formatted +
+      '</p>';
+    return html;
+  }
+  catch (error) {
+    console.log('theme_commerce_cart_line_item_review - ' + error);
+  }
+}
+
+/**
  * Theme the commerce cart buttons.
  */
 function reiscout_commerce_cart_buttons(variables) {
