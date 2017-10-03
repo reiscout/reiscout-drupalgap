c397d6db82205fd45b4a3512f6ab44f60d5c8384
Denis Popov <dzutaro@gmail.com>
2017-10-03 22:59:27 +0300

    Task #694683: update theme file and CSS rules

--- www/app/themes/reiscout/reiscout.js
+++ www/app/themes/reiscout/reiscout.js
@@ -1,7 +1,6 @@
 /**
  * Implements DrupalGap's template_info() hook.
  */
-
 function reiscout_info() {
   try {
     var theme = {
@@ -47,33 +46,6 @@ function reiscout_info() {
 /**
  * Themes a commerce cart line item.
  */
-function reiscout_commerce_cart_line_item(variables) {
-  try {
-    var html = '<h2>' + variables.line_item.line_item_title + '</h2>' +
-        '<p><strong>Price</strong>: ' + variables.line_item.commerce_unit_price_formatted + '</p>';
-    if (variables.line_item.type != 'shipping') {
-      /*
-       html += theme('commerce_cart_line_item_quantity', {
-       line_item: variables.line_item,
-       order: variables.order
-       }) +
-       */
-      html += theme('commerce_cart_line_item_remove', {
-        line_item: variables.line_item,
-        order: variables.order
-      });
-    }
-    html += '<p class="ui-li-aside"><strong>Total</strong>: ' +
-        variables.line_item.commerce_total_formatted +
-        '</p>';
-    return html;
-  }
-  catch (error) { console.log('theme_commerce_cart_line_item - ' + error); }
-}
-
-/**
- * Themes a commerce cart line item.
- */
 function reiscout_commerce_cart_line_item_review(variables) {
   try {
     var quantity = Math.floor(variables.line_item.quantity);
@@ -86,27 +58,6 @@ function reiscout_commerce_cart_line_item_review(variables) {
     return html;
   }
   catch (error) {
-    console.log('theme_commerce_cart_line_item_review - ' + error);
+    console.log('reiscout_commerce_cart_line_item_review - ' + error);
   }
-}
-
-/**
- * Theme the commerce cart buttons.
- */
-function reiscout_commerce_cart_buttons(variables) {
-  try {
-    var html =
-        theme('button_link', {
-          text: 'Checkout',
-          path: 'checkout/' + variables.order.order_id,
-          options: {
-            attributes: {
-              'data-icon': 'check',
-              'data-theme': 'b'
-            }
-          }
-        });
-    return html;
-  }
-  catch (error) { console.log('theme_commerce_cart_buttons - ' + error); }
 }
