ab310842f284537742d14eb9268e56107c663f1d
Denis Popov <dzutaro@gmail.com>
2017-10-04 01:05:14 +0300

    Update theme file

--- www/app/themes/reiscout/reiscout.js
+++ www/app/themes/reiscout/reiscout.js
@@ -46,6 +46,31 @@ function reiscout_info() {
 /**
  * Themes a commerce cart line item.
  */
+function theme_commerce_cart_line_item(variables) {
+  try {
+    var html = '<h2>' + variables.line_item.line_item_title + '</h2>' +
+      '<p><strong>Price</strong>: ' + variables.line_item.commerce_unit_price_formatted + '</p>';
+    if (variables.line_item.type != 'shipping') {
+      html += theme('commerce_cart_line_item_quantity', {
+          line_item: variables.line_item,
+          order: variables.order
+        }) +
+        theme('commerce_cart_line_item_remove', {
+          line_item: variables.line_item,
+          order: variables.order
+        });
+    }
+    html += '<p class="ui-li-aside"><strong>Total</strong>: ' +
+      variables.line_item.commerce_total_formatted +
+      '</p>';
+    return html;
+  }
+  catch (error) { console.log('theme_commerce_cart_line_item - ' + error); }
+}
+
+/**
+ * Themes a commerce cart line item.
+ */
 function reiscout_commerce_cart_line_item_review(variables) {
   try {
     var quantity = Math.floor(variables.line_item.quantity);
