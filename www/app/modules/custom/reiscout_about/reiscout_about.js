ce7a9d746088a2107c8beaa510abee3f4a290aad
Denis Popov <dzutaro@gmail.com>
2018-06-18 12:15:50 +0300

    Display the current year on About page

--- www/app/modules/custom/reiscout_about/reiscout_about.js
+++ www/app/modules/custom/reiscout_about/reiscout_about.js
@@ -18,9 +18,10 @@ function reiscout_about_menu() {
  */
 function reiscout_about_page() {
   try {
+    var current_year = new Date().getFullYear();
     var html = '<div id="title">' + drupalgap.settings.app.name + ' ' + drupalgap.settings.app.version + '</div>';
 
-    html += '<div id="copyright">Copyright © 2017 Three Oaks Technology Group, LLC</div>';
+    html += '<div id="copyright">Copyright © 2017-' + current_year + ' Three Oaks Technology Group, LLC</div>';
     html += '<div id="url"><a href="#" onclick="javascript:window.open(\'https://reiscout.com\', \'_system\');">www.reiscout.com</a></div>';
     html += '<div id="url_privacy"><a href="#" onclick="javascript:window.open(\'https://reiscout.com/privacy-policy\', \'_system\');">Privacy Policy</a></div>';
     html += '<div id="url_terms"><a href="#" onclick="javascript:window.open(\'https://reiscout.com/terms-of-use\', \'_system\');">Terms of Use</a></div>';
